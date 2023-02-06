0aaa8b40de6ac9f7aefaab0ed8d6499f4adb3494
keple <keplerde@gmail.com>
2023-02-06 21:00:45 +0100

    First, it was : - Two apps: products_app, users_app - Path - Views - Templates - + Static

---
+++ store/products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# функции = контроллеры = вьюхи
+
+def index(request):
+    return render(request, 'products\index.html')
+
+
+def products(request):
+    return render(request, 'products\products.html')    # дублировать строки CTRL + D
