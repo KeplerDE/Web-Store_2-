9ce53fd947148649e3fea383bb24b07ab23d6de6
keple <keplerde@gmail.com>
2023-02-09 16:24:19 +0100

    Step three: - DRY: Don't Repeat Yourself - {% load static %}

--- store/store/settings.py
+++ store/store/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 # Additional locations of static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static"),            # +
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
