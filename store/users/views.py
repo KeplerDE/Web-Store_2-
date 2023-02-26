cb0062424d32aa334f8a808dcef1736141065d96
keple <keplerde@gmail.com>
2023-02-26 18:44:33 +0100

    UserLoginForm() views .authenticate README

--- store/users/views.py
+++ store/users/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
 from users.models import User
 from users.forms import UserLoginForm
 
 
 def login(request):
-    context = {'form': UserLoginForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)  # пост запрос через форму, аудит
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)  # аутентификация, достаём данные из БД чтобы понять есть ли такой пользователь
+            if user:
+                auth.login(request, user)        # авторизация
+                return HttpResponseRedirect('/')  # авторизовались и перенаправились на главную
+
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 
