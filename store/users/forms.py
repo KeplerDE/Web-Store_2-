e8758c77bf323577d9c5959db4153955eaa2d31a
keple <keplerde@gmail.com>
2023-02-27 07:18:06 +0100

    Forms.py / views.py / registration.html - creating a signup form and checking user creation

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,10 +1,41 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User                # связываем форму с нашей моделью
+from django import forms
+#from users.tasks import send_email_verification
 
 
 #https://github.com/django/django/blob/2276ec8c21655b05bb44e14e236b499aa5d01f5b/django/contrib/auth/forms.py#L18
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+
+
     class Meta:
         model = User
         fields = ('username', 'password')
 
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    # def save(self, commit=True):
+    #     user = super(UserRegistrationForm, self).save(commit=True)
+    #     send_email_verification.delay(user.id)
+    #     return user
